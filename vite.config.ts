15330a5d4a2335ad67fe3afe3f1e03bc13a00027
AKhugo <hugo0258@protonmail.com>
2026-03-26 21:17:19 +0000

    build: Remove 404.html from Vite Rollup input configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: "./index.html",
-                "404": "./404.html",
             }
         }
     },
